9976fc4b1f21f21e24b642bd537ce5bae1d0aec8
Renato Setoue <renatosetoue@gmail.com>
2026-08-13 16:33:30 -0300

    added exercism test command

---
+++ ex-test/src/index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env bun
+
+import path from "path";
+import fs from "fs";
+
+const folderName = path.basename(process.cwd());
+const exerciseFile = `${folderName}.ts`;
+
+if (!fs.existsSync(`./${exerciseFile}`)) {
+	console.error(`\n❎ ${exerciseFile} file not found`);
+	process.exit(1);
+}
+
+console.log(`\n🚧 running tests: ${exerciseFile} \n`);
+
+const submit = Bun.spawnSync(["corepack", "yarn", "test"], {
+	stdio: ["inherit", "inherit", "inherit"],
+});
+
+if (submit.exitCode !== 0) {
+	console.error("\n❎ test command has failed!");
+	process.exit(1);
+}
+
+console.log(`\n✅ testing completed!`);
